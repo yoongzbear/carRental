75485b8c85b7fef327025731db47cae032566776
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-05-01 14:07:03 +0800

    Update User.java

--- User.java
+++ User.java
@@ -8,10 +8,6 @@ package SubangsCarRental;
  *
  * @author User
  */
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import javax.swing.JOptionPane;
 
 public class User {
     protected String email;
@@ -51,43 +47,4 @@ public class User {
     }
 }
 
-class Customer extends User {
-    // Attributes specific to the Customer class
-    private final String phoneNumber; // Phone number of the customer
-    private final String icNumber; // Identification number of the customer
-    private final String driNumber; // Driver's license number of the customer
 
-    // Constructor
-    public Customer(String name, String email, String phoneNumber, String icNumber,
-                    String driNumber, String password) {
-        super(email, name, password); // Call the constructor of the superclass (User)
-        this.phoneNumber = phoneNumber;
-        this.icNumber = icNumber;
-        this.driNumber = driNumber;
-    }
-
-    // Method to create an account for the customer
-    public void createAccount() {
-        // Validate input fields
-        if (name.isEmpty() || phoneNumber.isEmpty() || icNumber.isEmpty()
-                || driNumber.isEmpty()) {
-            System.out.println("Please fill in all the fields.");
-            return;
-        }
-
-        // Collect data
-        String account = name + "," + email + "," + phoneNumber + "," + icNumber + "," + driNumber + ","
-                + password;
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("account.txt", true))) {
-            writer.write(account);
-            writer.newLine();
-            System.out.println("Your account has been created");
-            JOptionPane.showMessageDialog(null, "Your account has been created", "Success", JOptionPane.INFORMATION_MESSAGE);
-            return;
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing to the file.");
-            e.printStackTrace();
-        }
-    }
-}
